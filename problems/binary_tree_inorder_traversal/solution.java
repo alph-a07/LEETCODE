095a4baf498940fad6f7c84bec9a42be4ef4d98c
Jeel <83648189+alph-a07@users.noreply.github.com>
2022-07-06 11:55:48 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (java)

--- problems/binary_tree_inorder_traversal/solution.java
+++ problems/binary_tree_inorder_traversal/solution.java
@@ -1,48 +1,61 @@
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// left root right
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        // empty tree
-        if (root == null)
-            return new ArrayList<>();
-
+        List<Integer> ans = new LinkedList<>();
         Stack<TreeNode> stack = new Stack<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        while (true) {
-            // if current node is not null then push it to stack
-            // go to its left node
-            if (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
 
-            // if the current node is null i.e. reached end
-            else {
-                // if reached end stack is also empty then all elements are added
-                // break the loop
-                if (stack.isEmpty())
-                    break;
+        TreeNode pointer = root;
 
-                // go to stack and add the element as it will be last left element
-                root = stack.pop();
-                list.add(root.val);
-                // go to right node
-                root = root.right;
+        while (!stack.isEmpty() || pointer != null) {
+            if (pointer != null) {
+                stack.push(pointer);
+                pointer = pointer.left;
+            } else {
+                TreeNode temp = stack.pop();
+                ans.add(temp.val);
+                pointer = temp.right;
             }
         }
-        return list;
+
+        return ans;
     }
 }

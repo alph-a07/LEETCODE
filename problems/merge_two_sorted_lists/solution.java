907263c618e41517ef0c2e98c1fca15e36c040c9
Jeel <83648189+alph-a07@users.noreply.github.com>
2022-07-08 17:45:54 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (java)

--- problems/merge_two_sorted_lists/solution.java
+++ problems/merge_two_sorted_lists/solution.java
@@ -1,24 +1,26 @@
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
+    // NOT IN-PLACE
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode ans = new ListNode();
-        ListNode temp1 = list1, temp2 = list2, temp3;
-         if(list1==null&&list2==null)
-            return null;
-        if(list1!=null&&list2==null)
-            return list1;
-        if(list2!=null&&list1==null)
-            return list2;
-    
+        ListNode ans = new ListNode(); // answer list
+        ListNode temp1 = list1, temp2 = list2, temp3; // pointers
+
+        // Edge cases
+        if (list1 == null && list2 == null) return null;
+        if (list1 != null && list2 == null) return list1;
+        if (list2 != null && list1 == null) return list2;
+
+        // make lower value head as answer head
         if (list1.val <= list2.val) {
             ans.val = list1.val;
             temp1 = list1.next;
@@ -26,9 +28,13 @@ class Solution {
             ans.val = list2.val;
             temp2 = list2.next;
         }
-        temp3 = ans;
+
+        temp3 = ans; // pointer to answer list
+
+        // traverse though both lists
         while (temp1 != null && temp2 != null) {
             ListNode nn;
+            // attach smaller value node to answer list
             if (temp1.val <= temp2.val) {
                 nn = new ListNode(temp1.val);
                 temp3.next = nn;
@@ -38,15 +44,34 @@ class Solution {
                 temp3.next = nn;
                 temp2 = temp2.next;
             }
-            temp3 = nn;
+            temp3 = nn; // move temp3 forward
+        }
 
+        // attach if any nodes left
+        if (temp1 != null) {
+            temp3.next = temp1;
         }
-        if(temp1!=null){
-            temp3.next=temp1;
+        if (temp2 != null) {
+            temp3.next = temp2;
         }
-        if(temp2!=null){
-            temp3.next=temp2;
-        }
+
         return ans;
     }
+
+    /*public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }*/
 }

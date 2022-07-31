30aae0da2acb83082d04b1bd5c960bd16a332eaa
Jeel <83648189+alph-a07@users.noreply.github.com>
2022-07-31 09:12:08 +0000

    Sync LeetCode submission - Missing Number (java)

--- problems/missing_number/solution.java
+++ problems/missing_number/solution.java
@@ -1,10 +1,13 @@
 class Solution {
+    // Time complexity = O(N)
+    // Space complexity = O(1)
     public int missingNumber(int[] nums) {
-        int sum = (nums.length * (nums.length + 1)) / 2;
+        int sum = (nums.length * (nums.length + 1)) / 2; // sum of 1 to n
 
+        // Traverse array and subtract each term from sum
         for (int num : nums) {
             sum = sum - num;
         }
-        return sum;
+        return sum; // sum = missing element
     }
 }

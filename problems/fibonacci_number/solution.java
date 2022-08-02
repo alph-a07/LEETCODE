2370704602f5a873028253bb037a30ce5db9dbb0
Jeel <83648189+alph-a07@users.noreply.github.com>
2022-08-02 13:23:28 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- problems/fibonacci_number/solution.java
+++ problems/fibonacci_number/solution.java
@@ -1,19 +1,22 @@
 class Solution {
     public int fib(int n) {
+        // edge case
         if (n == 0)
             return 0;
-        
-        int[] array = new int[2]; // array of size 2
 
-        // base case fill
-        array[0] = 0;
-        array[1] = 1;
+        int[] storage = new int[2]; // 0 -> n
 
-        for (int slide = 1; slide <= n - 1; slide++) {
-            int sum = array[0] + array[1]; // get sum
-            array[0] = array[1]; // move to next slide
-            array[1] = sum; // sum at 2nd position
+        // starting window
+        storage[0] = 0;
+        storage[1] = 1;
+
+        // storage filling
+        for (int i = 2; i <= n; i++) {
+            int sum = storage[0] + storage[1]; // next number
+            storage[0] = storage[1]; // shift left
+            storage[1] = sum; // next number
         }
-        return array[1];
+
+        return storage[1];
     }
 }

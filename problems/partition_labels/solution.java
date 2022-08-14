7a6ff5e82fef15db9a3b9d009ae638d427f74b77
Jeel <83648189+alph-a07@users.noreply.github.com>
2022-08-14 11:01:20 +0000

    Sync LeetCode submission - Partition Labels (java)

--- problems/partition_labels/solution.java
+++ problems/partition_labels/solution.java
@@ -8,9 +8,10 @@ class Solution {
         int max = 0;
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
-            int cMax = s.lastIndexOf(c);
-            max = Math.max(max, cMax);
+            int cMax = s.lastIndexOf(c); // min length needed for current character
+            max = Math.max(max, cMax); // min length needed for all 0-i characters
 
+            // partition will be made at MAX
             if (i == max) {
                 res.add(i - start + 1);
                 start = i + 1;

15ac149790f3dbb81852d366ab5ad6c5098fa7d2
Jeel <83648189+alph-a07@users.noreply.github.com>
2022-08-22 10:24:03 +0000

    Sync LeetCode submission - Unique Binary Search Trees II (java)

---
+++ problems/unique_binary_search_trees_ii/solution.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public List<TreeNode> generateTrees(int n) {
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> res = new LinkedList<TreeNode>();
+
+        if (start > end) {
+            res.add(null); // empty tree
+            return res;
+        }
+
+        // making each number node and generating BSTs
+        for (int i = start; i <= end; i++) {
+            // In BST, left nodes are smaller and right are larger
+            List<TreeNode> left = generateTrees(start, i - 1);
+            List<TreeNode> right = generateTrees(i + 1, end);
+
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}

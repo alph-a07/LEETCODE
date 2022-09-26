437c219ae2688d65c63d550faeb1cf082c14291c
Jeel <83648189+alph-a07@users.noreply.github.com>
2022-09-26 09:27:29 +0000

    Sync LeetCode submission - Search a 2D Matrix (java)

--- problems/search_a_2d_matrix/solution.java
+++ problems/search_a_2d_matrix/solution.java
@@ -1,10 +1,13 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
+        // Row-wise search
         for (int row = 0; row < matrix.length; row++) {
+            // Check last element
             if (matrix[row][matrix[row].length - 1] == target)
                 return true;
 
+            // Check the row if last element is greater than target
             else if (matrix[row][matrix[row].length - 1] > target) {
                 for (int col = 0; col < matrix[row].length; col++) {
                     if (matrix[row][col] == target)
